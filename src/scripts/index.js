af48e88f5729c82499182293e4122194daf145b9
Adi Purnomo <adipurnomo811@gmail.com>
2021-05-13 12:20:04 +0700

    Create: content & navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,24 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+import data from '../DATA.json';
+
+const restoElement = document.querySelector('catalouge-item');
+restoElement.setAttribute("class", "restaurant");
+data.restaurants.forEach(item => {
+	// let description = item.description.splice(0, 20);
+	const card = document.createElement("card");
+	card.setAttribute("class", "card");
+	card.innerHTML = `
+	<div class="card-thumbnail">
+		<img class="thumbnail" src="${item.pictureId}" alt="${item.name}">
+	</div>
+	<div class="card-description">
+		<h4 class="card-optional"> ${item.city} • ${item.rating} </h4>
+		<h3 class="card-title"> ${item.name} </h3>
+		<p> ${item.description.substring(0, 150)} </p>
+	</div>
+	`;
+	
+	restoElement.appendChild(card);
+})
